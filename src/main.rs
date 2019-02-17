0a37adfcce3ce5f81b49a26dcb7e728828c18357
Flaviu Toader <flaviu.toader@gmail.com>
2019-02-17 15:14:34 +0100

    Add stock buttons

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,13 @@ extern crate gio;
 extern crate gtk;
 
 use gio::{ApplicationExt, ApplicationExtManual, ApplicationFlags};
-use gtk::{Application, ApplicationWindow, GtkWindowExt, WidgetExt};
+use gtk::{
+    Application, ApplicationWindow, ContainerExt, GtkWindowExt, SeparatorToolItem, ToolButton,
+    Toolbar, WidgetExt,
+};
 use std::env;
+
+const PLAY_STOCK: &str = "gtk-media-play";
 
 fn main() {
     let application = Application::new("com.github.rbe-rusic", ApplicationFlags::empty())
@@ -11,7 +16,29 @@ fn main() {
     application.connect_startup(|application| {
         let window = ApplicationWindow::new(&application);
         window.set_title("Rusic");
-        window.show();
+
+        let toolbar = Toolbar::new();
+        window.add(&toolbar);
+
+        let open_button = ToolButton::new_from_stock("gtk-open");
+        toolbar.add(&open_button);
+        toolbar.add(&SeparatorToolItem::new());
+        let previous_button = ToolButton::new_from_stock("gtk-media-previous");
+        toolbar.add(&previous_button);
+        let play_button = ToolButton::new_from_stock(PLAY_STOCK);
+        toolbar.add(&play_button);
+        let stop_button = ToolButton::new_from_stock("gtk-media-stop");
+        toolbar.add(&stop_button);
+        let next_button = ToolButton::new_from_stock("gtk-media-next");
+        toolbar.add(&next_button);
+        toolbar.add(&SeparatorToolItem::new());
+        let remove_button = ToolButton::new_from_stock("gtk-remove");
+        toolbar.add(&remove_button);
+        toolbar.add(&SeparatorToolItem::new());
+        let quit_button = ToolButton::new_from_stock("gtk-quit");
+        toolbar.add(&quit_button);
+
+        window.show_all();
     });
     application.connect_activate(|_| {});
     application.run(&env::args().collect::<Vec<_>>());
