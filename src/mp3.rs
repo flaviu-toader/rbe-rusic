f0a0fde07db230c4601f804bd874d1815618c002
Flaviu Toader <flaviu.toader@gmail.com>
2019-05-05 20:21:17 +0200

    Added some MP3 decoder code

--- src/mp3.rs
+++ src/mp3.rs
@@ -1,18 +1,24 @@
+use simplemad;
 use std::io::{Read, Seek, SeekFrom};
 use std::time::Duration;
-use simplemad;
 
 use crate::to_millis;
 
-pub struct Mp3Decoder<R> where R: Read {
-    reader:  simplemad::Decoder<R>,
+pub struct Mp3Decoder<R>
+where
+    R: Read,
+{
+    reader: simplemad::Decoder<R>,
     current_frame: simplemad::Frame,
     current_frame_channel: usize,
     current_frame_sample_pos: usize,
     current_time: u64,
 }
 
-impl<R> Mp3Decoder<R> where R: Read + Seek {
+impl<R> Mp3Decoder<R>
+where
+    R: Read + Seek,
+{
     pub fn new(mut data: R) -> Result<Mp3Decoder<R>, R> {
         if !is_mp3(data.by_ref()) {
             return Err(data);
@@ -46,17 +52,75 @@ impl<R> Mp3Decoder<R> where R: Read + Seek {
         }
 
         let decoder = simplemad::Decoder::decode_headers(data).unwrap();
-        Some(decoder.filter_map(|frame| {
-            match frame {
-                Ok(frame) => Some(frame.duration),
-                Err(_) => None,
-            }
-        }).sum())
+        Some(
+            decoder
+                .filter_map(|frame| match frame {
+                    Ok(frame) => Some(frame.duration),
+                    Err(_) => None,
+                })
+                .sum(),
+        )
+    }
+}
+
+fn next_sample<R: Read>(decoder: &mut Mp3Decoder<R>) -> Option<i16> {
+    if decoder.current_frame.samples[0].len() == 0 {
+        return None;
+    }
+
+    // getting the sample and converting it from fixed step to i16
+    let sample = decoder.current_frame.samples[decoder.current_frame_channel]
+        [decoder.current_frame_sample_pos];
+    
+    let sample = sample.to_i32() + (1 << (28 - 16));
+    let sample = if sample >= 0x10000000 {
+        0x10000000 - 1
+    } else if sample <= -0x10000000 {
+        -0x10000000
+    } else {
+        sample
+    };
+    let sample = sample >> (28 + 1 - 16);
+    let sample = sample as i16;
+
+    decoder.current_frame_channel += 1;
+
+    if decoder.current_frame_channel < decoder.current_frame.samples.len() {
+        return Some(sample);
+    }
+
+    decoder.current_frame_channel = 0;
+    decoder.current_frame_sample_pos += 1;
+
+    if decoder.current_frame_sample_pos < decoder.current_frame.samples[0].len() {
+        return Some(sample);
+    }
+
+    decoder.current_frame = next_frame(&mut decoder.reader);
+    decoder.current_frame_channel = 0;
+    decoder.current_frame_sample_pos = 0;
+    decoder.current_time += to_millis(decoder.current_frame.duration);
+
+    return Some(sample);
+}
+
+impl<R> Iterator for Mp3Decoder<R> where R: Read {
+    type Item = i16;
+
+    fn next(&mut self) -> Option<i16> {
+        next_sample(self)
+    }
+    
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        (self.current_frame.samples[0].len(), None)
     }
 }
 
 /// Utility function to check if a stream of data is an mp3 file
-fn is_mp3<R>(mut data: R) -> bool where R: Read + Seek {
+fn is_mp3<R>(mut data: R) -> bool
+where
+    R: Read + Seek,
+{
     let stream_pos = data.seek(SeekFrom::Current(0)).unwrap();
     let is_mp3 = simplemad::Decoder::decode(data.by_ref()).is_ok();
     data.seek(SeekFrom::Start(stream_pos)).unwrap();
@@ -65,8 +129,10 @@ fn is_mp3<R>(mut data: R) -> bool where R: Read + Seek {
 
 /// Decode the next frame of an MP3 file
 fn next_frame<R: Read>(decoder: &mut simplemad::Decoder<R>) -> simplemad::Frame {
-    decoder.filter_map(|f| f.ok()).next().unwrap_or_else(|| {
-        simplemad::Frame {
+    decoder
+        .filter_map(|f| f.ok())
+        .next()
+        .unwrap_or_else(|| simplemad::Frame {
             bit_rate: 0,
             layer: Default::default(),
             mode: Default::default(),
@@ -74,6 +140,5 @@ fn next_frame<R: Read>(decoder: &mut simplemad::Decoder<R>) -> simplemad::Frame 
             samples: vec![Vec::new()],
             position: Duration::from_secs(0),
             duration: Duration::from_secs(0),
-        }
-    })
+        })
 }
